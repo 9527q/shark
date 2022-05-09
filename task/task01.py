f44e0776dc0ffbde63bc3f92399307e58e8f6ef8
ike <liuyuqi@honganhome.com>
2022-05-10 00:57:04 +0800

    第一天的

--- task/task01.py
+++ task/task01.py
@@ -2,7 +2,7 @@
 作业第一天
 打开文件，读取首尾的二进制数据
 """
-from collections import deque
+import os
 from typing import Union
 
 ET = "\n"  # convert backslash for f-string
@@ -10,24 +10,20 @@ ET = "\n"  # convert backslash for f-string
 
 def read_bytes_head_tail(
     filepath: str, length: int = 64, /
-) -> tuple[list[int], list[int], int]:
+) -> tuple[list[int], list[int]]:
     """
     读取指定长度的文件的开头和结尾定长的二进制字节数据
-    返回开头的二进制数据、结尾的二进制数据、总的数据长度
+    返回开头的二进制数据、结尾的二进制数据
     """
-    print(f"开始读取文件 '{filepath}'，首尾各取 {length} 字节")
     if length <= 0:
-        return [], [], 0
+        return [], []
 
     with open(filepath, "rb") as f:
         head = list(f.read(length))
-        tail = deque(head, maxlen=length)
-        f_length = len(head)  # 初始长度为读取到的长度，可能小于输入的 length
-        while byte_data := f.read(1):
-            tail.append(byte_data[0])
-            f_length += 1
+        f.seek(-64, 2)
+        tail = list(f.read(64))
 
-    return head, list(tail), f_length
+    return head, tail
 
 
 def hex_show_data(data: list[Union[int, None]], line_len: int = 16, /):
@@ -54,15 +50,14 @@ def main():
     filepath = "./data01.jpg"  # 要读取的文件路径
     read_length = 64  # 读取头尾的长度
     show_line_length = 16  # 展示的时候每行多长
-    head, tail, f_length = read_bytes_head_tail(filepath, read_length)
-    print(f"文件大小：{f_length}B")
+    head, tail = read_bytes_head_tail(filepath, read_length)
+    f_size = os.path.getsize(filepath)
+    print(f"文件大小：{f_size}B")
     print("第一种，结尾字节左端对齐")
     hex_show_data(head + tail, show_line_length)
     print("第二种，结尾字节原样对齐")
-    print(f"结尾偏移量：{f_length % show_line_length}")
-    hex_show_data(
-        head + [None] * (f_length % show_line_length) + tail, show_line_length
-    )
+    print(f"结尾偏移量：{f_size % show_line_length}")
+    hex_show_data(head + [None] * (f_size % show_line_length) + tail, show_line_length)
 
 
 if __name__ == "__main__":
@@ -73,7 +68,6 @@ if __name__ == "__main__":
 #                  结果
 #
 
-# 开始读取文件 './data01.jpg'，首尾各取 64 字节
 # 文件大小：172190B
 # 第一种，结尾字节左端对齐
 # 00000000 FF D8 FF E0 00 10 4A 46 | 49 46 00 01 01 00 00 01
