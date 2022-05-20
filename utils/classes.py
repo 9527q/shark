844d9d54e5770c621e7f6d6381a351c29082d815
ike <liuyu_qi617@163.com>
2022-05-20 19:59:23 +0800

    升级基类功能

--- utils/classes.py
+++ utils/classes.py
@@ -1,26 +1,22 @@
 """类相关的功能"""
 from dataclasses import dataclass
+from typing import Union
 
 
 @dataclass
-class GetitemBase:
-    """
-    具有索引、切片功能数据型基类（必须用非负数）
-    索引接口、偏移量等必须在实例生成时指定
-    """
+class GetitemDataclass:
+    """具有索引、切片功能的数据型基类（只能用非负数）"""
 
-    GETITEM_BASE_OFFSET = 0  # 基础偏移量
-
-    item_api: bytes  # 数据切片接口
+    item_api: Union[bytes, str, list, tuple]  # 数据切片接口
     item_api_offset: int  # 数据切片接口偏移量
 
     def __getitem__(self, item):
         if isinstance(item, slice):
-            start = (item.start or 0) + self.GETITEM_BASE_OFFSET + self.item_api_offset
+            start = (item.start or 0) + self.item_api_offset
             if (stop := item.stop) is not None:
-                stop += self.GETITEM_BASE_OFFSET + self.item_api_offset
+                stop += + self.item_api_offset
             item = slice(start, stop)
         else:
-            item += self.GETITEM_BASE_OFFSET + self.item_api_offset
+            item += self.item_api_offset
 
         return self.item_api[item]
