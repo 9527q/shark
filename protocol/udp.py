b4b8fbb3fb3a67fc9182e75b460c6238ec01f5fa
ike <liuyu_qi617@163.com>
2022-05-25 19:14:54 +0800

    完善UDP解析

--- protocol/udp.py
+++ protocol/udp.py
@@ -3,3 +3,20 @@ from protocol.base import Protocol
 
 class Udp(Protocol):
     """UDP User Datagram Protocol（用户数据报协议）"""
+
+    HEADER_LEN = 8
+
+    @property
+    def source_port(self) -> int:
+        return int.from_bytes(self[:2], "big")
+
+    @property
+    def destination_port(self) -> int:
+        return int.from_bytes(self[2:4], "big")
+
+    @property
+    def total_len(self) -> int:  # 总长度，单位字节
+        return int.from_bytes(self[4:6], "big")
+
+    def show(self):
+        return f"{self.source_port}->{self.destination_port} {self.total_len} Bytes"
