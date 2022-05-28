aba41e48dfd78667734bd665e50f18a13408def3
ike <liuyu_qi617@163.com>
2022-05-28 15:06:25 +0800

    最新版：item功能放到基类里

--- protocol/base.py
+++ protocol/base.py
@@ -7,10 +7,12 @@
 - 底层协议可以 import 上层协议
 - 底层协议到上层协议的对应、转换、配置等，一律放在底层协议本身及其 parse_payload 中
 """
-from utils.classes import Getitem, classproperty
+from typing import Union
+
+from utils.classes import classproperty
 
 
-class Protocol(Getitem):
+class Protocol:
     """所有协议都继承自此类，声明了一些基础方法，子类可以重写之"""
 
     # 类型名称，默认类名大写，是一个类属性
@@ -19,3 +21,27 @@ class Protocol(Getitem):
 
     def parse_payload(self):  # 解析载荷
         return type("未实现", (Protocol,), {})(**self.gen_getitem_kw(self.HEADER_LEN))
+
+    def gen_getitem_kw(self, offset=0):
+        """得到 Getitem 类所需要的关键字参数"""
+        return {
+            "item_api": self.item_api,
+            "item_api_offset": self.item_api_offset + offset,
+        }
+
+    def __init__(
+        self,
+        *,
+        item_api: Union[bytes, str, list, tuple],
+        item_api_offset: int = 0,
+    ):
+        self.item_api = item_api
+        self.item_api_offset = item_api_offset
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            if (stop := item.stop) is not None:
+                stop += self.item_api_offset
+            return self.item_api[self.item_api_offset + (item.start or 0) : stop]
+        else:
+            return self.item_api[self.item_api_offset + item]
