d0efa5b410628b4f2ec519e018337f851ea02978
ike <liuyu_qi617@163.com>
2022-05-28 11:49:58 +0800

    把ARP的解析改成动态的

--- protocol/arp.py
+++ protocol/arp.py
@@ -1,7 +1,7 @@
-import struct
+from functools import cached_property
 
-from utils.convert import ip2str
 from protocol.base import Protocol
+from utils.convert import bytes2int, ip2str, mac2str
 
 
 class Arp(Protocol):
@@ -9,28 +9,43 @@ class Arp(Protocol):
     TYPE_ASK = 1  # 请求
     TYPE_RES = 2  # 应答
 
-    def __post_init__(self):
-        self.mac_len = mac_len = self[4]  # mac 地址长度
-        self.ip_len = ip_len = self[5]  # ip 地址长度
-        self.type = struct.unpack(">h", self[6:8])[0]  # type: int  # 1 请求，2 应答
-        addr = self[8 : 8 + 2 * mac_len + 2 * ip_len]  # 地址数据
-        self.source_mac = addr[:mac_len]
-        self.source_ip = addr[mac_len : mac_len + ip_len]
-        self.destination_mac = addr[mac_len + ip_len : 2 * mac_len + ip_len]
-        self.destination_ip = addr[2 * mac_len + ip_len : 2 * (mac_len + ip_len)]
+    @property
+    def type(self) -> int:  # 1 请求，2 应答
+        return bytes2int(self[6:8])
+
+    @cached_property
+    def mac_len(self) -> int:  # 单位字节
+        return self[4]
+
+    @cached_property
+    def ip_len(self) -> int:  # 单位字节
+        return self[5]
+
+    @property
+    def source_ip(self) -> bytes:
+        return self[8 + self.mac_len : 8 + self.mac_len + self.ip_len]
+
+    @property
+    def source_mac(self) -> bytes:
+        return self[8 : 8 + self.mac_len]
+
+    @property
+    def destination_mac(self) -> bytes:
+        return self[8 + self.mac_len + self.ip_len : 8 + 2 * self.mac_len + self.ip_len]
+
+    @property
+    def destination_ip(self) -> bytes:
+        return self[
+            8 + 2 * self.mac_len + self.ip_len : 8 + 2 * (self.mac_len + self.ip_len)
+        ]
+
+    def parse_payload(self):
         Arp.MAP[self.source_ip] = self.source_mac
         if self.type == self.TYPE_RES:
             Arp.MAP[self.destination_ip] = self.destination_mac
 
     def show(self) -> str:
         if self.type == self.TYPE_ASK:
-            return (
-                f"[ARP请求] {(self.source_ip, self.source_mac)}"
-                f" 查询 {ip2str(self.destination_ip)} 的MAC地址在哪里"
-            )
+            return f"查询 {ip2str(self.destination_ip)} 的MAC地址"
         else:
-            return (
-                f"[ARP响应] {(self.source_ip, self.source_mac)}"
-                f" 回复 {(self.destination_ip, self.destination_mac)}"
-                f"： {ip2str(self.source_ip)} 的MAC地址在我这里"
-            )
+            return f"响应 {ip2str(self.source_ip)} 的MAC地址 {mac2str(self.source_mac)}"
